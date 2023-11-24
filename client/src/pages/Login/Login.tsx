7bed3cdb65b732c8146091bcbab0143299ad892e
sharoon kp <sharoonkp267@gmail.com>
2023-11-24 20:30:45 +0530

    error handled

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -104,7 +104,7 @@ function Login() {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign in
+                            {isLoading ? "loading..." : "Sign In"}
                         </button>
                     </div>
                 </form>
